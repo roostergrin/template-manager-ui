dad902a8af7ee1f379e5d674994e3160705218bb
Gordon Lewis <glewis@berkeley.edu>
2024-09-18 14:53:10 -0700

    add json starters and the "new stinson"

--- src/modelGroups.ts
+++ src/modelGroups.ts
@@ -1,6 +1,7 @@
 // TODO: automate getting the acf export to get the model groups
 // TODO: make a pydantic model for the sitemap to generate a sitemap export
 export const initialModelGroups = {
+  'New Stinson': ['Accordion', 'Before After Slider', 'Block Grid', 'Blog Posts', 'Form', 'Hero', 'Image, Text', 'Image, No Text', 'Logo Banner', 'Map', 'Masonary Grid', 'Multi Item Row', 'Multi Item Testimonial', 'Multi Use Banner', 'Single Image Slider', 'Single Testimonial', 'Single Video Slider', 'Tabs', 'Text, No Image, Full Headline', 'Text, No Image, Simple'],
   'Stinson': ['Hero', 'Image, Text', 'Text, No Image, Simple', 'Text, No Image, Full Headline', 'Multi Item Row', 'Masonary Grid', 'Multi Item Testimonial', 'Single Testimonial', 'Multi Use Banner', 'Block Contact', 'Block Grid', 'Contact Form', 'Single Image Slider'],
 };
 
