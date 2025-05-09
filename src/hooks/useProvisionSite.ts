3b549dc8440379e811ed032a5fb318234fa57183
Gordon Lewis <glewis@berkeley.edu>
2025-05-09 10:46:14 -0700

    refactor useProvisionSite hook to use mutationFn for improved clarity and consistency

--- src/hooks/useProvisionSite.ts
+++ src/hooks/useProvisionSite.ts
@@ -3,7 +3,9 @@ import provisionSite from "../services/provisionSiteService";
 import { SiteProvision, SiteProvisionResponse } from "../types/APIServiceTypes";
 
 const useProvisionSite = () => {
-  const mutation = useMutation<SiteProvisionResponse, Error, SiteProvision>(provisionSite);
+  const mutation = useMutation<SiteProvisionResponse, Error, SiteProvision>({
+    mutationFn: provisionSite,
+  });
   return [mutation.data, mutation.status, mutation.mutateAsync] as [SiteProvisionResponse | undefined, MutationStatus, (req: SiteProvision) => Promise<SiteProvisionResponse>];
 };
 
